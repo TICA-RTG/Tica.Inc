a0c19942c1911eb7744300ed43f190cb707127c7
tica-rtg <grantrobert85@gmail.com>
2023-07-13 16:22:33 +0000

    1st commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+const person = {
+  FirstName:'Daniels',
+  LastName:'Rita',
+  Age : 22,
+  Hobbies : ['music', 'sports', 'dance'],
+  address: {
+    street: 'mattin-sowah',
+    city: 'Accra',
+    state: 'GtAccra'
+  }
+}
+
+//console.log`Items (${1+1}):   $${(2095+799)/100}`
+
